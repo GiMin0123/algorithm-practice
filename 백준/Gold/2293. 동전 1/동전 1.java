7ccf2131f0b9b596bed1b94773797269a1230f8e
GiMin0123 <111266826+GiMin0123@users.noreply.github.com>
2025-09-22 16:01:06 +0900

    [Gold IV] Title: 동전 1, Time: 108 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+   
+    public static void main(String[] args) throws IOException{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+
+       int n = Integer.parseInt(st.nextToken());
+       int k = Integer.parseInt(st.nextToken());
+
+       int[] arr = new int[n];
+
+       for(int i=0; i<n; i++){
+           arr[i] = Integer.parseInt(br.readLine().trim());
+       }
+
+       long[] dp = new long[k+1];
+       dp[0] = 1;
+       for(int coin: arr){
+           for(int x = coin; x<=k; x++){
+               dp[x]+= dp[x-coin];
+           }
+       }
+
+       System.out.println(dp[k]);
+    }
+}
